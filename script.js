73bf0b0453ad1367878dd2950d6e52de468f08bd
Keith Holman <holmak@gmail.com>
2021-06-20 08:44:35 -0700

    Added a canvas.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 "use strict";
 
 function main() {
+    const Zoom = 2;
+    const DisplayWidth = 160;
+    const DisplayHeight = 120;
+
     var textInput = document.getElementById("textInput");
     var textOutput = document.getElementById("textOutput");
+    var canvas = document.getElementById("pixelOutput");
+    var g = canvas.getContext("2d");
+    g.fillStyle = "#000000";
+    g.fillRect(0, 0, 320, 240);
 
     function onTextInput(event) {
         if (event.key == "Enter") {
             textOutput.innerHTML += ("\n" + textInput.value);
             textOutput.scrollTop = textOutput.scrollHeight * 2;
             textInput.value = "";
+
+            // Draw a pixel:
+            g.fillStyle = "#FFFFFF";
+            g.fillRect(0, 0, Zoom, Zoom);
         }
     }
 
